63b7dc2e1dc92cb87e98a091bb56088b177d8c47
Enrique Alvarez <ejarkm@gmail.com>
2024-02-26 23:33:06 +0000

    SPAIK-0001: Testing action

--- data/try_app.sh
+++ data/try_app.sh
@@ -2,4 +2,5 @@ curl -X 'POST' \
   'http://localhost:8000/parse-bill' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
+  -H 'INVOICE_API_KEY:111' \
   -d '{"image_url": "https://drive.google.com/uc?id=1EH8eRTR--_goRQGVjmCu3Dazzkh0EZE9", "bill_type": "iberdrola"}'
